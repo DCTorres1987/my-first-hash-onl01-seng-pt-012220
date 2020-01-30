18bfbfa80064843864d5a512a1722e150d1c10aa
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-30 00:00:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-students = {"name" => "Danielle Torres"
+students = {"name" => "Danielle Torres","program" => "Online Software Engineer", "pace" => "Part-Time"}
   
 end
 
